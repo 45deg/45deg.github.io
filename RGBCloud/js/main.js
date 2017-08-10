d03ff16568146fc835591670fe47bf61fff8d5d0
45deg <45deg@users.noreply.github.com>
2017-08-11 02:38:36 +0900

    Added HSL Mode

--- RGBCloud/js/main.js
+++ RGBCloud/js/main.js
@@ -50,7 +50,7 @@ function getHistogram(imageData){
   renderHistogram(histogram);
 }
 
-var scene, camera, controls, renderer, wireMat;
+var scene, camera, controls, renderer;
 function initVisualizer(){
   var container = document.getElementById('visualizer');
 
@@ -70,25 +70,81 @@ function initVisualizer(){
 	controls.panSpeed = 0.05;
 
   scene = new THREE.Scene();
-  var wireGeo = new THREE.EdgesGeometry( new THREE.CubeGeometry(256, 256, 256) );
-  wireMat = new THREE.LineBasicMaterial( { color: 0xffffff, linewidth: 1 } );
-  var wireFrame = new THREE.LineSegments( wireGeo, wireMat );
-  scene.add( wireFrame );
+  renderFrame('RGB');
 
   animate();
 }
 
+var wireMat = new THREE.LineBasicMaterial( { color: 0xffffff, opacity: 0.5, linewidth: 1 } );
+var frame = null;
+function renderFrame(type){
+  const SIZE = 256;
+
+  if(frame !== null)
+    scene.remove(frame);
+
+  if(type === 'RGB') {
+    // cube
+    var wireGeo = new THREE.EdgesGeometry( new THREE.CubeGeometry(SIZE, SIZE, SIZE) );
+    frame = new THREE.LineSegments( wireGeo, wireMat );
+    scene.add( frame );
+  } else if(type === 'HSL'){
+    // bicone
+    frame = new THREE.Group();
+
+    const circleSegments = 128, circleRadius = SIZE / 2;
+    var circleGeom = new THREE.Geometry();
+    var colors = [];
+    for(var i = 0; i < circleSegments; ++i){
+      var t = i / circleSegments;
+      circleGeom.vertices.push(
+        new THREE.Vector3( circleRadius * Math.cos(2 * Math.PI * t),
+                           circleRadius * Math.sin(2 * Math.PI * t),
+                           0 )
+      );
+      colors[i] = new THREE.Color();
+
+      console.log(t)
+      colors[i].setHSL(t, 1, 0.5);
+    }
+    circleGeom.colors = colors;
+    var circleMat =  new THREE.LineBasicMaterial(
+       { color: 0xffffff, opacity: 0.5,
+         linewidth: 1, vertexColors: THREE.VertexColors } );
+    frame.add( new THREE.LineLoop( circleGeom, circleMat ) );
+
+    var wires = 8;
+    for(var i = 0; i < wires; ++i){
+      var x = SIZE / 2 * Math.cos(2 * Math.PI / wires * i);
+      var y = SIZE / 2 * Math.sin(2 * Math.PI / wires * i);
+      var lineGeom = new THREE.Geometry();
+      lineGeom.vertices.push(new THREE.Vector3( 0, 0, SIZE / 2 ));
+      lineGeom.vertices.push(new THREE.Vector3( x, y, 0 ));
+      lineGeom.vertices.push(new THREE.Vector3( 0, 0, -SIZE / 2 ));
+      frame.add( new THREE.Line( lineGeom, wireMat ) );
+    }
+
+    scene.add( frame );
+  }
+}
+
 var currentPoints = null;
+var geometry;
 function renderHistogram(histogram, min, max){
   // clear children
   if(currentPoints !== null)
     scene.remove(currentPoints);
 
-  var geometry = new THREE.BufferGeometry();
+  geometry = new THREE.BufferGeometry();
   var particles = histogram.size;
+
 	var positions = new Float32Array( particles * 3 );
 	var colors = new Float32Array( particles * 3 );
   var sizes = new Float32Array( particles );
+
+  var spaceIndex = spaces.indexOf(spaceStatus);
+  var times = new Float32Array( particles ).fill(spaceIndex);
+
   var color = new THREE.Color();
   var cnt = 0;
 
@@ -96,9 +152,9 @@ function renderHistogram(histogram, min, max){
     var r = (key >> 16) & 255;
     var g = (key >> 8) & 255;
     var b = key & 255;
-    positions[cnt] = r - 128;
-    positions[cnt + 1] = g - 128;
-    positions[cnt + 2] = b - 128;
+    positions[cnt] = r;
+    positions[cnt + 1] = g;
+    positions[cnt + 2] = b;
 
     color.setRGB( r / 255, g / 255, b / 255 );
 		colors[cnt]     = color.r;
@@ -113,6 +169,7 @@ function renderHistogram(histogram, min, max){
 	geometry.addAttribute('position', new THREE.BufferAttribute( positions, 3 ) );
 	geometry.addAttribute('color', new THREE.BufferAttribute( colors, 3 ) );
   geometry.addAttribute('size', new THREE.BufferAttribute( sizes, 1 ));
+  geometry.addAttribute('time', new THREE.BufferAttribute( times, 1 ));
 	geometry.computeBoundingSphere();
 
 	//var material = new THREE.PointsMaterial( { size: 1, vertexColors: THREE.VertexColors } );
@@ -130,17 +187,39 @@ function renderHistogram(histogram, min, max){
 
 function animate() {
    requestAnimationFrame( animate );
+
    controls.update();
+
+   if(spaceTransition) {
+     // update transition
+     var limit = spaceTransition.limit;
+     var delta = spaceTransition.delta;
+     var time = spaceTransition.value + delta;
+     if(delta > 0 ? time > limit : time < limit) {
+       // finish
+       time = limit;
+       spaceTransition = null;
+     } else {
+       spaceTransition.value = time;
+     }
+     // update buffer
+     var timeAttr = geometry.attributes.time;
+     timeAttr.array.fill(time);
+     timeAttr.needsUpdate = true;
+   }
+
    renderer.render(scene, camera);
 }
 
 var currentColor = 'black';
 function toggleColor(){
   var box = document.getElementById('preview-container');
-  var button = document.getElementById('toggle-button');
+  var buttons = document.getElementsByTagName('button');
 
-  button.style.borderColor = box.style.borderColor = currentColor;
-  button.style.color = box.style.color = currentColor;
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].style.borderColor = box.style.borderColor = currentColor;
+    buttons[i].style.color = box.style.color = currentColor;
+  }
 
   renderer.setClearColor( new THREE.Color(currentColor == 'black' ? 0xFFFFFF: 0x000000) );
   wireMat.color = new THREE.Color(currentColor == 'black' ? 0x000000: 0xFFFFFF);
@@ -152,19 +231,39 @@ function toggleColor(){
   }
 }
 
+var spaces = ["RGB", "HSL"];
+var spaceStatus = spaces[0]; // index of spaces
+var spaceTransition = null;
+function toggleSpace() {
+  var index = (spaces.indexOf(spaceStatus) + 1) % spaces.length;
+  spaceStatus = spaces[index];
+  this.innerHTML = spaceStatus;
+  renderFrame(spaceStatus);
+
+  if(geometry) {
+    spaceTransition = {
+      value: 1 - index,
+      delta: index > 0 ? 0.05: -0.05,
+      limit: index,
+    };
+  }
+}
+
 window.addEventListener('load', function(){
   initFileReader();
   initVisualizer();
 
-  document.getElementById('toggle-button').addEventListener('click', function(){
+  window.addEventListener('resize', function(){
+  	var aspect = window.innerWidth / window.innerHeight;
+  	camera.aspect = aspect;
+  	camera.updateProjectionMatrix();
+
+  	renderer.setSize( window.innerWidth, window.innerHeight );
+  }, false);
+
+  document.getElementById('toggle-colour').addEventListener('click', function(){
     toggleColor();
   });
-}, false);
 
-window.addEventListener('resize', function(){
-	var aspect = window.innerWidth / window.innerHeight;
-	camera.aspect = aspect;
-	camera.updateProjectionMatrix();
-
-	renderer.setSize( window.innerWidth, window.innerHeight );
+  document.getElementById('toggle-space').addEventListener('click', toggleSpace);
 }, false);
